1529cace6dadca839338c5bcf173dfa100665e4a
namisha21 <87444416+namisha21@users.noreply.github.com>
2021-07-14 11:42:02 -0700

    Add files via upload

---
+++ vmss.tf
@@ -0,0 +1,298 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "${var.subscription_id}"
+}
+
+# number of instances to create
+locals {
+  instance_count = 2
+}
+
+#Resource group creation
+resource "azurerm_resource_group" "vmss" {
+ name     = var.resource_group_name
+ location = var.location
+ tags     = var.tags
+}
+
+resource "random_string" "fqdn" {
+ length  = 6
+ special = false
+ upper   = false
+ number  = false
+}
+
+# Virtual Network with a private subnet
+resource "azurerm_virtual_network" "vmss" {
+ name                = "vmss-vnet"
+ address_space       = ["10.0.0.0/16"]
+ location            = var.location
+ resource_group_name = azurerm_resource_group.vmss.name
+ tags                = var.tags
+}
+
+# private subnet and all required dependent infrastructure
+resource "azurerm_subnet" "vmss" {
+ name                 = "vmss-subnet"
+ resource_group_name  = azurerm_resource_group.vmss.name
+ virtual_network_name = azurerm_virtual_network.vmss.name
+ address_prefixes     = ["10.0.1.0/24"]
+}
+
+#Public IP creation
+resource "azurerm_public_ip" "vmss" {
+ name                         = "vmss-public-ip"
+ location                     = var.location
+ resource_group_name          = azurerm_resource_group.vmss.name
+ allocation_method = "Static"
+ domain_name_label            = random_string.fqdn.result
+ tags                         = var.tags
+}
+
+# Azure Load Balancer with web server instances
+resource "azurerm_lb" "vmss" {
+ name                = "vmss-lb"
+ location            = var.location
+ resource_group_name = azurerm_resource_group.vmss.name
+
+ frontend_ip_configuration {
+   name                 = "PublicIPAddress"
+   public_ip_address_id = azurerm_public_ip.vmss.id
+ }
+
+ tags = var.tags
+}
+
+# the backend pool
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+ resource_group_name = azurerm_resource_group.vmss.name
+ loadbalancer_id     = azurerm_lb.vmss.id
+ name                = "BackEndAddressPool"
+}
+
+# a simple health probe to make sure the web servers in the backend pool are responding and healthy
+resource "azurerm_lb_probe" "vmss" {
+ resource_group_name = azurerm_resource_group.vmss.name
+ loadbalancer_id     = azurerm_lb.vmss.id
+ name                = "ssh-running-probe"
+ port                = var.application_port
+}
+
+#load balancer rule
+resource "azurerm_lb_rule" "lbnatrule" {
+   resource_group_name            = azurerm_resource_group.vmss.name
+   loadbalancer_id                = azurerm_lb.vmss.id
+   name                           = "http"
+   protocol                       = "Tcp"
+   frontend_port                  = var.application_port
+   backend_port                   = var.application_port
+   backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+   frontend_ip_configuration_name = "PublicIPAddress"
+   probe_id                       = azurerm_lb_probe.vmss.id
+}
+
+# Virtual Machine Scale Set that launches Virtual Machines and registers them to the backend pool
+resource "azurerm_virtual_machine_scale_set" "vmss" {
+ name                = "vmscaleset"
+ location            = var.location
+ resource_group_name = azurerm_resource_group.vmss.name
+ upgrade_policy_mode = "Manual"
+
+ sku {
+   name     = "Standard_DS1_v2"
+   tier     = "Standard"
+   capacity = 2
+ }
+
+ storage_profile_image_reference {
+   publisher = "Canonical"
+   offer     = "UbuntuServer"
+   sku       = "16.04-LTS"
+   version   = "latest"
+ }
+
+ storage_profile_os_disk {
+   name              = ""
+   caching           = "ReadWrite"
+   create_option     = "FromImage"
+   managed_disk_type = "Standard_LRS"
+ }
+
+ storage_profile_data_disk {
+   lun          = 0
+   caching        = "ReadWrite"
+   create_option  = "Empty"
+   disk_size_gb   = 10
+ }
+
+ # Install Nginx and hello world program mentioned in file HelloServerNginx.conf
+ os_profile {
+   computer_name_prefix = "vmlab"
+   admin_username       = var.admin_user
+   admin_password       = var.admin_password
+   custom_data          = file("HelloServerNginx.conf")
+ }
+
+ os_profile_linux_config {
+   disable_password_authentication = false
+ }
+
+ network_profile {
+   name    = "terraformnetworkprofile"
+   primary = true
+
+   ip_configuration {
+     name                                   = "IPConfiguration"
+     subnet_id                              = azurerm_subnet.vmss.id
+     load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+     primary = true
+   }
+ }
+
+ tags = var.tags
+}
+
+#Network Security Group allowing HTTP traffic to load balancer
+resource "azurerm_network_security_group" "nsg" {
+  name                = "Assign-Security-Group"
+  location            = azurerm_resource_group.vmss.location
+  resource_group_name = azurerm_resource_group.vmss.name
+
+  security_rule {
+    name                       = "http-rule"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+#association of NSG to subnet
+resource "azurerm_subnet_network_security_group_association" "nsga" {
+  subnet_id                 = azurerm_subnet.vmss.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+#public IP jumpbox
+resource "azurerm_public_ip" "jumpbox" {
+ name                         = "jumpbox-public-ip"
+ location                     = var.location
+ resource_group_name          = azurerm_resource_group.vmss.name
+ allocation_method = "Static"
+ domain_name_label            = "${random_string.fqdn.result}-ssh"
+ tags                         = var.tags
+}
+
+#NIC jumpbox
+resource "azurerm_network_interface" "jumpbox" {
+ name                = "jumpbox-nic"
+ location            = var.location
+ resource_group_name = azurerm_resource_group.vmss.name
+
+ ip_configuration {
+   name                          = "IPConfiguration"
+   subnet_id                     = azurerm_subnet.vmss.id
+   private_ip_address_allocation = "dynamic"
+   public_ip_address_id          = azurerm_public_ip.jumpbox.id
+ }
+
+ tags = var.tags
+}
+
+#Establish an Autoscale rule that scales up/down based on a metric of your choice (and be able to demonstrate a scaling event)
+resource "azurerm_monitor_autoscale_setting" "mas" {
+  name                = "autoscale-cpu"
+  target_resource_id  = "${azurerm_virtual_machine_scale_set.vmss.id}"
+  location            = "${azurerm_resource_group.vmss.location}"
+  resource_group_name = "${azurerm_resource_group.vmss.name}"
+
+  profile {
+    name = "autoscale-cpu"
+
+    capacity {
+      default = "${local.instance_count}"
+      minimum = 0
+      maximum = 100
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = "${azurerm_virtual_machine_scale_set.vmss.id}"
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = "${azurerm_virtual_machine_scale_set.vmss.id}"
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 15
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+}
+
+#virtual machine jumpbox
+resource "azurerm_virtual_machine" "jumpbox" {
+ name                  = "jumpbox"
+ location              = var.location
+ resource_group_name   = azurerm_resource_group.vmss.name
+ network_interface_ids = [azurerm_network_interface.jumpbox.id]
+ vm_size               = "Standard_DS1_v2"
+
+ storage_image_reference {
+   publisher = "Canonical"
+   offer     = "UbuntuServer"
+   sku       = "16.04-LTS"
+   version   = "latest"
+ }
+
+ storage_os_disk {
+   name              = "jumpbox-osdisk"
+   caching           = "ReadWrite"
+   create_option     = "FromImage"
+   managed_disk_type = "Standard_LRS"
+ }
+
+ os_profile {
+   computer_name  = "jumpbox"
+   admin_username = var.admin_user
+   admin_password = var.admin_password
+ }
+
+ os_profile_linux_config {
+   disable_password_authentication = false
+ }
+
+ tags = var.tags
+}
